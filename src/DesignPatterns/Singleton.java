d918bb22aca9f076acbd52ccba58aeae129a145b
Pratik Agarwal Sinhal <pasinhal@gmail.com>
2019-11-21 15:18:26 +0530

    Singleton pattern in java

---
+++ src/DesignPatterns/Singleton.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Singleton
+{
+    private static final long serialVersionUID = 1L;
+ 
+    private Singleton() {
+		throw new IllegalStateException("instance already created");
+    }
+ 
+    private static class InstanceHolder {
+    	private static final Singleton instance = new Singleton();
+    }
+ 
+    public static Singleton getInstance() {
+    	return InstanceHolder.instance;
+    }
+ 
+    protected Object readResolve() {
+    	return getInstance();
+    }
+ 
+    protected Object clone() throws CloneNotSupportedException {
+    	//throw new CloneNotSupportedException("Cloning of this class is not allowed");
+    	return getInstance();
+    }
+ 
+	public static void main(String[] args) {
+ 
+	}
+}
